e7fda213cf0a94c1f739a17ea69b42fb653bcf84
Your Name <you@example.com>
2018-09-13 16:46:44 +0530

    selenium code

---
+++ src/test/java/example/NewTest4.java
@@ -0,0 +1,41 @@
+package example;		
+
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;				
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;
+import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeOptions;
+public class NewTest4 {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {
+			System.setProperty("webdriver.chrome.driver","/home/edureka/chromedriver/chromedriver");
+			ChromeOptions chromeOptions = new ChromeOptions();
+			WebDriver driver = new ChromeDriver(chromeOptions);
+			System.out.println("Hi, Welcome to Edureka's YouTube Live session on Selenium WebDriver");
+			driver.get("http://localhost:8888/addressbook");
+			driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+			driver.findElement(By.className("v-button")).click();
+			driver.findElement(By.id("gwt-uid-5")).sendKeys("Edureka");
+			driver.findElement(By.id("gwt-uid-7")).sendKeys("DevOps");
+			driver.findElement(By.id("gwt-uid-9")).sendKeys("9834562709");
+			driver.findElement(By.id("gwt-uid-11")).sendKeys("Devops@edureka.co");
+			driver.findElement(By.id("gwt-uid-13")).sendKeys("01/01/2015");
+			driver.findElement(By.className("v-button-primary")).click(); 
+			//Thread.sleep(5000); 
+			driver.quit();
+					
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new ChromeDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
